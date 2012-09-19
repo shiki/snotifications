8478e477b95ff07fe34890c8611f264981e28992
Shiki <shikishiji@gmail.com>
2012-09-19 16:26:08 +0800

    In SNotificationsComponent: checking if observers is available first before looping. This will allow setting the notifications as a Yii component but not giving any initial observers.

--- SNotificationsComponent.php
+++ SNotificationsComponent.php
@@ -13,7 +13,7 @@ class SNotificationsComponent extends CApplicationComponent
 {
   /**
    * Must be in this format:
-   * 
+   *
    * array(
    *   '<event-name>' => array(
    *     array('<class-name>', '<method-name>'),
@@ -25,31 +25,33 @@ class SNotificationsComponent extends CApplicationComponent
    *     array('<class-name>', '<method-name>')
    *   )
    * )
-   * 
+   *
    * Observer methods must have the signature `methodName(SNotification $notification)`
-   * 
+   *
    * @var array
    */
   public $observers;
-  
+
   /**
    *
    * @var SNotifications
    */
   protected $_default;
-  
+
   /**
-   * @return SNotifications 
+   * @return SNotifications
    */
   public function getDefault()
   {
     if (!$this->_default) {
       $this->_default = new SNotifications();
-      foreach ($this->observers as $key => $value) {
-        $this->_default->addObservers($key, $value);
+      if (is_array($this->observers)) {
+        foreach ($this->observers as $key => $value) {
+          $this->_default->addObservers($key, $value);
+        }
       }
     }
-    
+
     return $this->_default;
   }
 }
